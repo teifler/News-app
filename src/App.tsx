9679ce3134007afb691bd08e1e2ffa98aa5dbb6f
Tim Eifler <tim.eif@protonmail.com>
2022-04-22 17:58:41 +0200

    Add fetch of news articles :construction:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { EverythingRootObject } from './interfaces/everyting_interface';
 
-function App() {
+const App: React.FC = () => {
+  const API_KEY: string = '6e769055c0fa4086924abb5f8af1bc8a';
+  const [searchQuery, setSearchQuery] = useState<String>('bitcoin');
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [newsArticleList, setNewsArticleList] = useState<
+    EverythingRootObject[]
+  >([]);
+  const url: string = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${API_KEY}`;
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const { articles } = await response.json();
+        setNewsArticleList(articles);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+      setLoading(false);
+    };
+    fetchNews();
+  }, [url]);
+
+  if (loading) {
+  }
+
   return (
     <div className="App">
       <h1>News</h1>
+      {error && <h3>Please reload the page there was an error</h3>}
     </div>
   );
-}
+};
 
 export default App;
