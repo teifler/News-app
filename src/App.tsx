c3d9a466e57dda36b9d10140fc4bc993c1c96ac5
Tim Eifler <tim.eif@protonmail.com>
2022-04-24 14:53:25 +0200

    Add component SearchInputForm :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import { EverythingRootObject } from './interfaces/everyting_interface';
 
+import loadingSpinner from './images/loadingSpinner.svg';
+
 const App: React.FC = () => {
-  const API_KEY: string = '6e769055c0fa4086924abb5f8af1bc8a';
+  //const API_KEY: string = '6e769055c0fa4086924abb5f8af1bc8a';
   const [searchQuery, setSearchQuery] = useState<String>('bitcoin');
   const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [newsArticleList, setNewsArticleList] = useState<
     EverythingRootObject[]
   >([]);
-  const url: string = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${API_KEY}`;
+  //const url: string = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${API_KEY}`;
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         setLoading(true);
-        const response = await fetch(url);
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${process.env.REACT_APP_API_KEY}`
+        );
         const { articles } = await response.json();
         setNewsArticleList(articles);
       } catch (error) {
@@ -26,17 +32,38 @@ const App: React.FC = () => {
       setLoading(false);
     };
     fetchNews();
-  }, [url]);
+  }, [searchQuery]);
+
+  console.log(newsArticleList);
 
   if (loading) {
+    return (
+      <LoadingContainer>
+        <img src={loadingSpinner} alt="loading..." height="80" width="80"></img>
+      </LoadingContainer>
+    );
   }
 
   return (
     <div className="App">
       <h1>News</h1>
-      {error && <h3>Please reload the page there was an error</h3>}
+      {error && (
+        <h3>
+          Please reload the page there was an error while fetching the data
+        </h3>
+      )}
     </div>
   );
 };
+
+const LoadingContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transform: -webkit-translate(-50%, -50%);
+  transform: -moz-translate(-50%, -50%);
+  transform: -ms-translate(-50%, -50%);
+`;
 
 export default App;
---
+++ src/components/SearchInputForm.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const SearchInputForm: React.FC<{
+  setSearchQuery: (arg0: string) => void;
+}> = ({ setSearchQuery }) => {
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [validationError, setValidationError] = useState<boolean>(false);
+  const onSubmit = (): void => {
+    setSearchQuery(searchInput);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const trimmed: string = e.target.value.trim();
+    if (trimmed !== '' && !trimmed.match(/^[a-zA-Z0-9_. ]+$/)) {
+      setValidationError(true);
+    } else {
+      setSearchInput(trimmed);
+      setValidationError(false);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+      }}
+    >
+      <FlexContainer>
+        <SearchInput
+          onChange={handleChange}
+          aria-label="Searchinput"
+          placeholder="Search here..."
+        ></SearchInput>
+        <SubmitButton
+          type="button"
+          aria-label="Submit"
+          onClick={onSubmit}
+          disabled={validationError}
+        >
+          Search
+        </SubmitButton>
+      </FlexContainer>
+      {validationError && (
+        <SearchValidationError>
+          Please don't use special characters
+        </SearchValidationError>
+      )}
+    </form>
+  );
+};
+
+export default SearchInputForm;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SearchInput = styled.input`
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 10px 0 0 10px;
+  border-right: none;
+  outline: none;
+  font-size: 20px;
+  color: black;
+  background: none;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #acbdba;
+  text-align: center;
+  height: 51px;
+  width: 80px;
+  outline: none;
+  cursor: pointer;
+  border-radius: 0 10px 10px 0;
+  border: 1px solid black;
+  border-left: none;
+  font-size: 20px;
+  border-left: 2px solid black;
+`;
+
+const SearchValidationError = styled.p`
+  color: red;
+`;
